b0b4f75fc98739c1e19bf4ef97e0f9961d241a8a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-21 22:33:08 +0800

    Update: Aug 21 2024

--- Day1-10/Day1to10.py
+++ Day1-10/Day1to10.py
@@ -16,7 +16,7 @@ def GetAge() :
 
     getAge = input("Enter your age: ")
 
-    if int(getAge) >= 18 : 
+    if int(getAge) >= 18 and int(getAge) <= 25: 
         print("Your Teenager")
-    elif int(getAge) >= 18 :
+    elif int(getAge) <= 18 :
         print("Your Teenager") 
