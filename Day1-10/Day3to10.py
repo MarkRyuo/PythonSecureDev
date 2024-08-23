130aa3111988e4bfeb9bf60a3a0c757c37aa794e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-23 17:49:35 +0800

    Update: Aug 23 2024

--- Day1-10/Day3to10.py
+++ Day1-10/Day3to10.py
@@ -45,9 +45,9 @@ def getName() :
     if getname: 
         for i in list_ofName :
             print(i)
-            getaddname.append(getname)
-            
+            getaddname.append(getname)       
     else :
+        return False 
 
 if __name__ == "__main__" :
 
