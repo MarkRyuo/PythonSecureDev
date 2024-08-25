873fd76704726debdce1df6a2ef5edb450119f88
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-25 17:05:18 +0800

    Update: Aug 25 2024

--- Day1-10/Day5to10.py
+++ Day1-10/Day5to10.py
@@ -18,7 +18,8 @@ def loopofname() :
     for values in dict_of_name.values() : #* Looping in values of dictionaries  
         print(values) 
     
-    for key in dict_of_name.keys()
+    for key in dict_of_name.keys() :
+        print(key)
 
 
 
