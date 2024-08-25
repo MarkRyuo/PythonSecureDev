906916c63b762224708b15bfb092eb42222d2f73
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-25 16:54:44 +0800

    Update: Aug 25 2024

--- Day1-10/Day5to10.py
+++ Day1-10/Day5to10.py
@@ -2,3 +2,5 @@
 #* Loop in dict 
 #* Methods to loop a dict:  Key(), values(), items() 
 #* Key() -  gets you the keys in a dictionary
+#* values() - gets you the values in a dictionary 
+#* items() - gets you both the keys and values in a dictionary 
