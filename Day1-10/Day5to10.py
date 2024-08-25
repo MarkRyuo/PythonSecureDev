ba97b11ff724441045db4c070592b991833d3e56
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-25 17:05:01 +0800

    Update: Aug 25 2024

--- Day1-10/Day5to10.py
+++ Day1-10/Day5to10.py
@@ -1,7 +1,7 @@
 
 #* Loop in dict 
 #* Methods to loop a dict:  Key(), values(), items() 
-#* Key() -  gets you the keys in a dictionary
+#* keys() -  gets you the keys in a dictionary
 #* values() - gets you the values in a dictionary 
 #* items() - gets you both the keys and values in a dictionary 
 
@@ -18,6 +18,7 @@ def loopofname() :
     for values in dict_of_name.values() : #* Looping in values of dictionaries  
         print(values) 
     
+    for key in dict_of_name.keys()
 
 
 
