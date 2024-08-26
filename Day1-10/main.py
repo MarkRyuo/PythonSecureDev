8befcf2ccdb22ee59350112d21eca578eebde759
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-27 06:54:35 +0800

    Update: Aug 27 2024

--- Day1-10/main.py
+++ Day1-10/main.py
@@ -6,39 +6,6 @@
 
 # todo create a data by name of characters in the Temporal Incident
 
-data = {
-    "Char1": {
-        "username" : "Moda",
-        "age" : 21,
-        "alive" : False
-    },
-
-    "Char2" : {
-        "username" : "Niyari",
-        "age" : 19,
-        "alive": True
-    },
-    
-    "Char3" : {
-        "username" : "Riyuo",
-        "age" : 19,
-        "alive" : False
-    },
-
-    "Char4" : {
-        "username" : "Samantya",
-        "age" : 19,
-        "alive" : True
-    },
-
-    "Char5" : {
-        "username" : "Jiyan",
-        "age" : 19,
-        "alive" : True
-    },
-    
-
-}
 
 #* Check if the character in the data is alive, by inputting the name.
 
