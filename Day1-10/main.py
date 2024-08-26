5468c88ae08586f211039c95cf25e0af1201c4cb
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 17:58:52 +0800

    Update: Aug 26 2024

--- Day1-10/main.py
+++ Day1-10/main.py
@@ -50,10 +50,12 @@ def check_alive() :
     for key, value in data.items() :
         print(f"{key}, {value}")
         if value["username"] == charname :
+
             if value["alive"] :
                 print(f"{charname} is alive")
             else :
                 print(f"{charname} is unknown")
+            
         else :
             return f"{charname} is not found"
 
