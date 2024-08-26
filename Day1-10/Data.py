db2dff7bdb874faeacf597f40eb82befc91c8776
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-27 06:54:23 +0800

    Update: Aug 27 2024

---
+++ Day1-10/Data.py
@@ -0,0 +1,34 @@
+
+data = {
+    "Char1": {
+        "username" : "Moda",
+        "age" : 21,
+        "alive" : False
+    },
+
+    "Char2" : {
+        "username" : "Niyari",
+        "age" : 19,
+        "alive": True
+    },
+    
+    "Char3" : {
+        "username" : "Riyuo",
+        "age" : 19,
+        "alive" : False
+    },
+
+    "Char4" : {
+        "username" : "Samantya",
+        "age" : 19,
+        "alive" : True
+    },
+
+    "Char5" : {
+        "username" : "Jiyan",
+        "age" : 19,
+        "alive" : True
+    },
+    
+
+}
