0f1a77739597e1f1e76e95acd13a542c1c2b62c5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-26 17:12:32 +0800

    Update: Aug 26 2024

--- Day1-10/main.py
+++ Day1-10/main.py
@@ -43,6 +43,9 @@ data = {
 
 #* Check if the character in the data is alive, Input the name 
 
-def check_alive() :
+def check_alive() -> str :
 
-    charname = input("")  
+    charname = input("Enter the name: ") 
+
+
+    return charname 
