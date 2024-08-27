b275d8dc951f5bd2e813f1133b786934cb83f32e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-28 00:29:55 +0800

    Update: Aug 27 2024

--- src/Main.py
+++ src/Main.py
@@ -5,5 +5,9 @@ from Call import GetCharacter
 if __name__ == '__main__' :
     char = GetCharacter()
     print(char.Charloop())
+    
+    askQ1 = str(input("Do you want to display the data? (Y/n): "))
+    
+    if 
     char.CallName()
 
