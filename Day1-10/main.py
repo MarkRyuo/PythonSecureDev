fb06c561f9bec7c1e1361bc5c26a3c76dc7e75f3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-27 17:57:09 +0800

    Update: Aug 27 2024

--- Day1-10/main.py
+++ Day1-10/main.py
@@ -41,6 +41,8 @@ def get_username() -> str :
         if getName: 
             data.append(getName)
             print("-1")
+        else : 
+            return "Username can't be empty!"
 
 
     
