a54ecb89494e0360ec4b455881afb91b6ca5165e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-27 22:48:54 +0800

    Update: Aug 27 2024

--- src/Components/Data.py
+++ src/Components/Data.py
@@ -1,9 +1,28 @@
 
 
+#* Create a Nested Dict Module
 
 data = dict({
     
     "User1" : {
-        "Uname" : "Niyari"
-    }
+        "Uname" : "Niyari",
+        "Uage" : 19
+    },
+    
+    "User2" : {
+        "Uname" : "Riyuo",
+        "Uage" : 19
+    },
+    
+    "User3" : {
+        "Uname" : "Samantya",
+        "Uage" : 19
+    },
+    
+    "User4" : {
+        "Uname" : "Niyari",
+        "Uage" : 21
+    },
+    
+    
 })
